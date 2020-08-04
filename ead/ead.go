65a3c6969e570b338ee879fe5c8fa2925b9409dc
David <da70@nyu.edu>
2020-08-04 18:46:11 -0400

    ead/ead.go: add new field Barcode to Container

--- ead/ead.go
+++ ead/ead.go
@@ -98,6 +98,7 @@ type Change struct {
 
 type Container struct {
 	AltRender string `xml:"altrender,attr" json:"altrender,omitempty"`
+	Barcode   string `json:"barcode,omitempty"`
 	ID        string `xml:"id,attr" json:"id,omitempty"`
 	Label     string `xml:"label,attr" json:"label,omitempty"`
 	Parent    string `xml:"parent,attr" json:"parent,omitempty"`
