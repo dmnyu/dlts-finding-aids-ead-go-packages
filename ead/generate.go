d11453c43d39de89aeb31274372b33ad26887652
David <da70@nyu.edu>
2020-09-03 17:41:05 -0400

    ead/: generate DAO and PhysDesc MarshalJSON methods using generate.go

--- ead/generate.go
+++ ead/generate.go
@@ -35,6 +35,35 @@ const convertTextWithTagsMarshalJSONCodeTemplate = `func ({{.VarName}} *{{.TypeN
 	return jsonData, nil
 }`
 
+const omitWhitespaceOnlyValueFieldsMarshalJSONCodeTemplate = `func ({{.VarName}} *{{.TypeName}}) MarshalJSON() ([]byte, error) {
+	type {{.TypeName}}WithNoWhitespaceOnlyValues {{.TypeName}}
+
+	containsNonWhitespace, err := regexp.MatchString(` + "`\\S`" + `, {{.VarName}}.Value)
+	if err != nil {
+		return nil, err
+	}
+
+	var value string
+	if containsNonWhitespace {
+		value = {{.VarName}}.Value
+	} else {
+		value = ""
+	}
+
+	jsonData, err := json.Marshal(&struct {
+		Value string ` + "`" + `json:"value,chardata,omitempty"` + "`" + `
+		*{{.TypeName}}WithNoWhitespaceOnlyValues
+	}{
+		Value: value,
+		{{.TypeName}}WithNoWhitespaceOnlyValues: (*{{.TypeName}}WithNoWhitespaceOnlyValues)({{.VarName}}),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return jsonData, nil
+}`
+
 func main() {
 	w := new(bytes.Buffer)
 
@@ -44,9 +73,11 @@ package ead
 
 import (
 	"encoding/json"
+	"regexp"
 )`)
 
 	writeConvertTextWithTagsCodeToBuffer(w)
+	writeOmitWhitespaceOnlyValueFieldsCodeToBuffer(w)
 
 	// Format with gofmt
 	out, err := format.Source(w.Bytes())
@@ -92,6 +123,32 @@ func writeConvertTextWithTagsCodeToBuffer(w *bytes.Buffer) {
 
 		err := t.Execute(w, templateData{
 			ConversionFunction : conversionFunction,
+			TypeName: typeName,
+			VarName:  strings.ToLower(typeName),
+		})
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func writeOmitWhitespaceOnlyValueFieldsCodeToBuffer(w *bytes.Buffer) {
+	type templateData struct{
+		TypeName string
+		VarName string
+	}
+
+	t := template.Must(template.New("").Parse(omitWhitespaceOnlyValueFieldsMarshalJSONCodeTemplate))
+
+	sortedTypes := []string{
+		"DAO",
+		"PhysDesc",
+	}
+
+	for _, typeName := range sortedTypes {
+		w.WriteString("\n\n")
+
+		err := t.Execute(w, templateData{
 			TypeName: typeName,
 			VarName:  strings.ToLower(typeName),
 		})
