33c164490d3a9b2ef91cafcb0598b9b59dfce71f
jgpawletko <jgpawletko@gmail.com>
2021-08-04 18:40:33 -0400

    reorganize testdata/, update tests

--- ead/ead_test.go
+++ ead/ead_test.go
@@ -10,7 +10,7 @@ import (
 	"time"
 )
 
-const testFixturePath string = "./testdata/v0.1.2"
+const testFixturePath string = "./testdata/omega/v0.1.2"
 
 func failOnError(t *testing.T, err error, label string) {
 	if err != nil {
