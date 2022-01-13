7a9d951e1bd65e4058057e43ab02ccaa7e8b3fdb
jgpawletko <jgpawletko@gmail.com>
2022-01-12 19:25:05 -0500

    update version to 0.10.0

--- ead/ead.go
+++ ead/ead.go
@@ -5,7 +5,7 @@ package ead
 // Based on: "Data model for parsing EAD <archdesc> elements": https://jira.nyu.edu/jira/browse/FADESIGN-29.
 
 const (
-	Version = "0.9.0"
+	Version = "0.10.0"
 )
 
 type EAD struct {
